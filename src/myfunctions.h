b7a455379dca956e60f8e7c73383e67deccfabea
Symbat Bekzhigit <sb7971@nyu.edu>
2022-07-18 12:55:45 +0600

    Create myfunctions.h

---
+++ src/myfunctions.h
@@ -0,0 +1,12 @@
+
+#include<iostream>
+#include<cstdlib>
+#include<sstream>
+
+#include "contact.h"
+using namespace std;
+
+
+Contact* generateNewContact();
+string generateKey(string fname, string lname);
+string removeSpaces(string str);
